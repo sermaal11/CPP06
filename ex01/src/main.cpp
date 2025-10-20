55e64d53b155fde4bc7e26cb07f5971f8d1e8148
Jariskan <sergio.marin.alvarez@gmail.com>
2025-10-20 11:03:05 +0200

    Actualizar comentarios y mejorar mensajes de salida en main.cpp

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:02:43 by sergio            #+#    #+#             */
-/*   Updated: 2025/10/20 11:00:13 by sergio           ###   ########.fr       */
+/*   Updated: 2025/10/20 11:02:28 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main()
     std::cout << "    Valor entero (decimal) -> " << YELLOW << raw << RESET << '\n';
     std::cout << "    Valor entero (hex)     -> "
               << YELLOW << "0x" << std::hex << raw << std::dec << RESET << '\n';
-    std::cout << '\n';
+	std::cout << '\n';
 
     std::cout << MAGENTA << "[3] Deserializar el entero con Serializer::deserialize(raw)" << RESET << '\n';
     Data* back = Serializer::deserialize(raw);
@@ -42,11 +42,11 @@ int main()
 
     if (back == &data) 
 	{
-        std::cout << GREEN << "    ✔ OK: Son la misma dirección" << RESET << '\n';
+        std::cout << GREEN << "    OK: Son la misma dirección" << RESET << '\n';
     } 
 	else 
 	{
-        std::cout << RED   << "    ✘ ERROR: Direcciones distintas" << RESET << '\n';
+        std::cout << RED   << "    ERROR: Direcciones distintas" << RESET << '\n';
         return 1;
     }
     std::cout << '\n';
